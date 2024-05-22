686294ac6dbc6a35ac309d343968b249a11b3d42
gdewael <gaetandewaele@hotmail.com>
2024-05-22 09:50:39 +0000

    citeseq script update

--- bento_sc/scripts/pred_citeseq.py
+++ bento_sc/scripts/pred_citeseq.py
@@ -1,6 +1,6 @@
 from bento_sc.data import BentoDataModule
 from bento_sc.utils.config import Config
-from bento_sc.models import CLSTaskTransformer
+from bento_sc.models import CLSTaskTransformer, BentoTransformer
 from bento_sc.baselines import CLSTaskBaseline
 from lightning.pytorch.callbacks import ModelCheckpoint, EarlyStopping
 from lightning.pytorch.loggers import TensorBoardLogger
@@ -8,7 +8,7 @@ from lightning.pytorch import Trainer
 import sys
 
 config_path = str(sys.argv[1])
-baseline = str(sys.argv[2])
+approach = str(sys.argv[2])
 logs_path = str(sys.argv[3])
 no = str(sys.argv[4])
 
@@ -19,12 +19,21 @@ dm = BentoDataModule(
 )
 dm.setup(None)
 
-if baseline == "baseline":
+if approach == "baseline":
     model = CLSTaskBaseline(config)
+elif approach == "None":
+    model = CLSTaskTransformer(config)
 else:
-    model = CLSTaskTransformer(
-        config
-    )
+    model = CLSTaskTransformer(config)
+    pretrained_model = BentoTransformer.load_from_checkpoint(approach)
+
+    pretrained_dict = pretrained_model.state_dict()
+    model_dict = model.state_dict()
+    pretrained_dict_new = {
+        k: v for k, v in pretrained_dict.items() if not k.startswith(("nce_loss", "ct_clf_loss", "loss"))
+    }
+    model_dict.update(pretrained_dict_new)
+    model.load_state_dict(model_dict)
 
 val_ckpt = ModelCheckpoint(monitor="val_loss", mode="min")
 callbacks = [val_ckpt, EarlyStopping(monitor="val_loss", patience=10, mode="min")]
@@ -36,9 +45,9 @@ logger = TensorBoardLogger(
 
 trainer = Trainer(
     accelerator="gpu",
-    devices=[2],
+    devices=config.devices,
     strategy="auto",
-    max_epochs=10,
+    max_epochs=100,
     gradient_clip_val=1,
     callbacks=callbacks,
     logger=logger,
